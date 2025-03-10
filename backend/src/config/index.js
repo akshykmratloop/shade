6ac8586314cb580ec01a2aa9df4ef6a10f5918f8
Anukool <anukool.singh@loopmethods.com>
2025-03-10 11:45:54 +0530

    backend error push

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,6 +1,6 @@
 import dbConfig from "./dbConfig.js";
 import useModules from "./init.js";
-import { logger, morganMiddleware } from "./logconfig.js";
+import { logger, morganMiddleware } from "./logConfig.js";
 import emailTransporter from "./emailConfig.js";
 
 export { dbConfig, useModules, logger, morganMiddleware, emailTransporter };
