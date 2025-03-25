1c729c5d544306ffcabd62a4172680547ebd98cb
Anukool <anukool.singh@loopmethods.com>
2025-03-25 11:55:07 +0530

    Integration Complete for user update and show details, changed svg invert for dark mode

--- dashboard/src/features/Resources/components/PageDetails.jsx
+++ dashboard/src/features/Resources/components/PageDetails.jsx
@@ -28,13 +28,17 @@ const PageDetails = ({ data, display, setOn }) => {
 
     return (
         <div className={`${display ? "block" : "hidden"} fixed z-20 top-0 left-0 w-[100vw] h-screen bg-black bg-opacity-50`}>
-            <div ref={pageRef} className="fixed z-30 top-0 right-0 w-[26rem] h-screen bg-[white] dark:bg-[#242933] shadow-xl-custom">
-                <button className="bg-transparent hover:bg-stone-900 hover:text-stone-200 dark:hover:bg-stone-300 rounded-full absolute top-7 border border-gray-500 dark:border-stone-700 left-4 p-2 py-2"
+            <div ref={pageRef} className="fixed z-20 top-0 right-0 w-[26rem] h-screen bg-[white] dark:bg-[#242933] shadow-xl-custom">
+                <button className="bg-transparent hover:bg-stone-900 hover:text-stone-200 dark:hover:bg-stone-900 rounded-full absolute top-7 border border-gray-500 dark:border-stone-700 left-4 p-2 py-2"
                     onClick={() => setOn(false)}>
                     <X className="w-[16px] h-[16px]" />
                 </button>
                 <h1 className="font-medium text-[1.1rem] shadow-md-custom p-[30px] text-center">Assign User for Page {data.heading}</h1>
-                <div className=" flex flex-col h-[93%] text-[14px] custom-text-color p-[30px] pt-0 mt-2">
+                <div className=" flex flex-col h-[93%] text-[14px] custom-text-color p-[30px] pt-0 mt-2 overflow-y-scroll customscroller">
+                    <div className="flex py-[15px] justify-between border-b dark:border-stone-700">
+                        <label>Request Number:</label>
+                        <p>2</p>
+                    </div>
                     <div className="flex py-[15px] justify-between border-b dark:border-stone-700">
                         <label>Last Edited:</label>
                         <p>09/09/2025</p>
@@ -87,19 +91,19 @@ const PageDetails = ({ data, display, setOn }) => {
                             </div>
                             <div className="flex justify-between">
                                 <div className="flex flex-col translate-x-[14px]">
-                                    <img src={Assigned} alt="" className="w-[20px] h-[20px] invert" />
+                                    <img src={Assigned} alt="" className="w-[20px] h-[20px] dark:invert" />
                                     <p>Assigned</p>
                                 </div>
                                 <div className="flex flex-col items-center translate-x-[12px]">
-                                    <img src={Edit} alt="" className="w-[20px] h-[20px] invert" />
+                                    <img src={Edit} alt="" className="w-[20px] h-[20px] dark:invert" />
                                     <p>Edit</p>
                                 </div>
                                 <div className="flex flex-col items-center translate-x-[12px]">
-                                    <img src={Verify} alt="" className="w-[20px] h-[20px] invert" />
+                                    <img src={Verify} alt="" className="w-[20px] h-[20px] dark:invert" />
                                     <p>Verify ({"Level 2"}) </p>
                                 </div>
                                 <div className="flex flex-col items-center translate-x-[0px]">
-                                    <img src={Publisher} alt="" className="w-[20px] h-[20px] invert" />
+                                    <img src={Publisher} alt="" className="w-[20px] h-[20px] dark:invert" />
                                     <p>Publish</p>
                                 </div>
                             </div>
