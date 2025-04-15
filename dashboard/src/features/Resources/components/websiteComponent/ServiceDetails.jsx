bb4234b40e4b815819279e1e3dff1881c3b3c615
Anukool <anukool.singh@loopmethods.com>
2025-04-15 11:06:45 +0530

    service details page UI

--- dashboard/src/features/Resources/components/websiteComponent/ServiceDetails.jsx
+++ dashboard/src/features/Resources/components/websiteComponent/ServiceDetails.jsx
@@ -4,7 +4,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { updateContent } from "../../../common/homeContentSlice";
 import { services, projectPageData } from "../../../../assets/index";
 import content from './content.json'
-import structureOfServiceDetails from "./structures/structureOFServiceDetails.json"
+import structureOfServiceDetails from "./structures/structureOFServiceDetails.json";
+import { TruncateText } from "../../../../app/capitalizeword";
+
 const ServiceDetails = ({ serviceId, contentOn, language, screen }) => {
     const dispatch = useDispatch();
     const isComputer = screen > 1100;
@@ -18,11 +20,7 @@ const ServiceDetails = ({ serviceId, contentOn, language, screen }) => {
         (item) => item?.id == serviceId
     )[0];
 
-    console.log(currentContent)
-
     useEffect(() => {
-
-
         if (!currentContent?.[serviceId]) {
             dispatch(updateContent({ currentPath: "serviceDetails", payload: [...content.serviceDetails, { ...structureOfServiceDetails, id: contentFromRedux?.length + 1 }] }))
         } else {
@@ -30,7 +28,8 @@ const ServiceDetails = ({ serviceId, contentOn, language, screen }) => {
         }
     }, [])
     return (
-        <div dir={isLeftAlign ? "ltr" : "rtl"}>
+        <div dir={isLeftAlign ? "ltr" : "rtl"} className="w-full">
+            {/* banner */}
             <section className={`py-[120px] px-20 bg-center object-cover text-center flex flex-col items-center`}
                 style={{ backgroundImage: `linear-gradient(to bottom,#00000020 ,#fffffffb 100%), url(${services.contructionTowerImage})`, backgroundPosition: "bottom" }}
             >
@@ -42,22 +41,65 @@ const ServiceDetails = ({ serviceId, contentOn, language, screen }) => {
                 </p>
             </section>
 
-            <section>
-                <article singleService>
-                    <div>
-                        <img src={""} alt="" />
-                    </div>
-                    <article>
-                        <h3 className="text-[17px]">Project Service 1</h3>
-                        <p className="text-[11px]">
-                        We capitalize on our years of experience in the construction industry to clients by also maintaining their facilities and infrastructure.
-                        </p>
-                        <button className="text-[#00B9F2]">
-                            Veiw Details
-                            <img src="" alt="" />
-                        </button>
-                    </article>
-                </article>
+            {/* Sub services */}
+            <section className={`grid grid-cols-2 gap-y-[27px] gap-x-[25px] px-[76px] py-[34px]`}>
+                {
+                    currentContent?.subServices?.map((subService, index) => {
+
+                        return (
+                            <article singleService className={`border-b flex gap-4 pb-[12px]`}>
+                                <article className={` w-[197px] py-2`}>
+                                    <img src={projectPageData.developmentOfHo} alt="" className={`w-[196px] h-[135px]`} />
+                                </article>
+                                <article className="w-[194px]">
+                                    <h3 className="text-[17px]">Project Service {index + 1}</h3>
+                                    <p className="text-[11px]">
+                                        We capitalize on our years of experience in the construction industry to clients by also maintaining their facilities and infrastructure.
+                                    </p>
+                                    <button className="text-[#00B9F2] text-[11px]">
+                                        Veiw Details
+                                        <img src="" alt="" />
+                                    </button>
+                                </article>
+                            </article>
+                        )
+                    })
+                }
+            </section>
+
+            {/* Other Services */}
+            <section className="w-[988px] overflow-x-scroll rm-scroll py-10">
+
+                <section dir={isLeftAlign ? 'ltr' : 'rtl'}
+                    className={`flex gap-10 ml-[76px] w-fit items`}>
+                    {currentContent?.otherServices?.map((service, idx) => {
+                        if (!service.display) return null
+                        return (
+                            <article
+                                key={idx}
+                                className="flex flex-col h-full bg-white \ overflow-hidden shadow w-[300px]"
+                            >
+                                <img src={service.image} alt="img" className="w-full object-cover h-[176px]" />
+                                <section className="bg-[#F8F8F8] py-[14px] px-[18px] flex flex-col justify-between flex-1">
+                                    <h1 className="text-[#292E3D] text-[22px] font-[400]">
+                                        {TruncateText(service?.title?.[language], isTablet ? 15 : 23)}
+                                    </h1>
+                                    <p className="text-[#292E3D] text-[10px] mb-2">
+                                        {service?.subtitle?.[language]}
+                                    </p>
+                                    <button className={`text-[#00B9F2] flex gap-1 items-center mt-auto ${!isLeftAlign && "flex-rows-reverse"}`}>
+                                        {service?.button?.[language]}
+                                        <img
+                                            src="https://frequencyimage.s3.ap-south-1.amazonaws.com/61c0f0c2-6c90-42b2-a71e-27bc4c7446c2-mingcute_arrow-up-line.svg"
+                                            alt=""
+                                            className={`${isLeftAlign && "rotate-[180deg]"} w-[16px] h-[16px]`}
+                                        />
+                                    </button>
+                                </section>
+                            </article>
+                        )
+                    })}
+                </section>
             </section>
         </div>
     )
