22fe08c691c140f3b8708a192d5bbcb0a4edcafc
Anukool <anukool.singh@loopmethods.com>
2025-06-19 13:08:30 +0530

    pulling from deepanshu_dev

--- backend/src/modules/website/website.service.js
+++ backend/src/modules/website/website.service.js
@@ -1,7 +1,9 @@
 import {logger} from "../../config/logConfig.js";
 import {fetchContentForWebsite} from "../../repository/website.repository.js";
+import { fetchContent } from "../../repository/content.repository.js";
 
 const getContentForWebsite = async (slug) => {
+  
   const content = await fetchContentForWebsite(slug);
   logger.info({
     response: "Content fetched successfully",
