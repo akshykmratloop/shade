e0f661d3c32d4e03091423d142c77657cdbb0068
Anukool <anukool.singh@loopmethods.com>
2025-06-19 15:27:20 +0530

    swiper correction

--- dashboard/src/features/Resources/components/websiteComponent/detailspages/SnRPolicies.jsx
+++ dashboard/src/features/Resources/components/websiteComponent/detailspages/SnRPolicies.jsx
@@ -45,7 +45,7 @@ const SnRPolicies = ({ currentContent, screen, language, width, highlight, liveC
                 <div className="container relative h-full flex items-center justify-end"
                 >
                     <div
-                        className={` ${isLeftAlign ? 'scale-x-[-1]' : ''} ${isPhone ? "w-full" : isTablet ? "" : ""} flex flex-col ${isPhone ? "items-start" : " space-y-4"} `}>
+                        className={` ${isLeftAlign ? 'scale-x-[-1]' : ''} w-full flex flex-col ${isPhone ? "items-start" : " space-y-4"} `}>
                         <h2 className={`text-[#292E3D] font-medium ${isPhone ? "text-[40px]" : isTablet ? "text-[45px]" : "text-[45px]"} tracking-[-3px] mb-4
                             ${checkDifference(currentContent?.['1']?.content?.title?.[language], liveContent?.['1']?.content?.title?.[language])}
                         `}
