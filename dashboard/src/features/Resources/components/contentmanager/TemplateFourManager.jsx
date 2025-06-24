4110e9553f7e1d761c7b6d420c908e9548269994
Anukool <anukool.singh@loopmethods.com>
2025-06-24 10:46:54 +0530

    template 4 has been completed

---
+++ dashboard/src/features/Resources/components/contentmanager/TemplateFourManager.jsx
@@ -0,0 +1,174 @@
+// import { useEffect, useState } from "react"
+import FileUploader from "../../../../components/Input/InputFileUploader"
+import ContentSection from "../breakUI/ContentSections"
+import { useDispatch, useSelector } from "react-redux"
+import { updateSubServiceDetailsPointsArray } from "../../../common/homeContentSlice"
+import DynamicContentSection from "../breakUI/DynamicContentSection";
+
+
+const skeletons = {
+    "5": {
+        "title": {
+            "ar": "",
+            "en": ""
+        },
+        "description": {
+            "ar": "",
+            "en": ""
+        },
+        "images": [
+            {
+                "url": "",
+                "order": 1,
+                "altText": {
+                    "ar": "",
+                    "en": ""
+                }
+            }
+        ]
+    }
+}
+
+const TemplateFourManager = ({ content, currentPath, language, indexes }) => {
+    const dispatch = useDispatch()
+
+    const context = useSelector(state => state.homeContent?.present?.content) || {}
+
+    const addExtraSummary = (sectionIndex, structure, cardIndex) => {
+        dispatch(updateSubServiceDetailsPointsArray(
+            {
+                insert: skeletons[structure],
+                section: "cards",
+                sectionIndex,
+                operation: 'add',
+                cardIndex
+            }
+        ))
+    }
+
+    return (
+        <div>
+            {/* reference doc */}
+            <FileUploader id={"Temp-Two-ID-Reference"} label={"Rerference doc"} fileName={"Upload your file..."} />
+
+            {
+                context?.id === "N" &&
+                <ContentSection
+                    currentPath={currentPath}
+                    Heading={"Page - Details"}
+                    inputs={[
+                        { input: "input", label: "Title English", updateType: "titleEn", value: context?.titleEn, dir: "ltr" },
+                        { input: "input", label: "Title Arabic", updateType: "titleAr", value: context?.titleAr, dir: "rtl" },
+                        ...(context?.id === "N" ? [{ input: "input", label: "Slug", updateType: "slug", value: context?.slug }] : []),
+                    ]}
+                    section={"page-details"}
+                    language={language}
+                />
+            }
+
+            {/** Hero Banner */}
+            <ContentSection
+                currentPath={currentPath}
+                Heading={"Banner"}
+                inputs={[
+                    { input: "input", label: "Heading/title", updateType: "title", value: content?.['1']?.content?.title?.[language] },
+                    { input: "textarea", label: "Description", updateType: "description", maxLength: 400, value: content?.['1']?.content?.description?.[language] },
+                ]}
+                inputFiles={[{ label: "Backround Image", id: "ServiceBanner", order: 1, url: content?.['1']?.content?.images?.[0]?.url }]}
+                section={"banner"}
+                language={language}
+                currentContent={content}
+                sectionIndex={indexes?.['1']}
+            />
+
+            <ContentSection
+                currentPath={currentPath}
+                Heading={"Gallery"}
+                inputFiles={
+                    content?.[2]?.content?.images?.map((e) => {
+                        return {
+                            label: "Images",
+                            id: `markDown-${e.order}`,
+                            order: e.order,
+                            url: e?.url
+                        }
+                    })
+                }
+                section={"banner"}
+                language={language}
+                currentContent={content}
+                sectionIndex={indexes?.['2']}
+            />
+
+            <ContentSection
+                currentPath={currentPath}
+                Heading={"Sub Heading"}
+                inputs={[
+                    { input: "input", label: "Heading/title", updateType: "title", value: content?.['2']?.content?.title?.[language] },
+                    { input: "textarea", label: "Description", updateType: "description", maxLength: 400, value: content?.['2']?.content?.description?.[language] },
+                ]}
+                section={"banner"}
+                language={language}
+                currentContent={content}
+                sectionIndex={indexes?.['3']}
+            />
+
+
+            <ContentSection
+                currentPath={currentPath}
+                Heading={"Mark Down"}
+                inputs={[
+                    { input: "input", label: "Heading/title", updateType: "title", value: content?.['5']?.content?.title?.[language] },
+                    { input: "richtext", label: "Description", updateType: "description", value: content?.['5']?.content?.description?.[language] },
+                ]}
+                section={"MarkDown"}
+                isBorder={false}
+                language={language}
+                currentContent={content}
+                sectionIndex={indexes?.['4']}
+            />
+
+            <div className="mt-4 border-b pb-3">
+                <h3 className={`font-semibold text-[1.25rem] mb-4`}>Cards</h3>
+                {
+                    content?.[5]?.content?.cards?.map?.((section, i) => {
+                        return (
+                            <DynamicContentSection
+                                key={i}
+                                currentPath={currentPath}
+                                subHeading={`Card ${(i + 1)}`}
+                                inputs={[
+                                    { input: "input", label: "Title", updateType: "title", value: section?.title?.[language] },
+                                    { input: "textarea", label: "Description", updateType: "description", maxLength: 400, value: section?.description?.[language] },
+                                ]}
+                                inputFiles={[{ label: "Icon", id: "ServiceBanner", order: 1, url: section?.images?.[0]?.url }]}
+                                index={i}
+                                isBorder={false}
+                                section={"cards"}
+                                subSection={"cards"}
+                                language={language}
+                                currentContent={content}
+                                sectionIndex={indexes?.['5']}
+                                contentIndex={i}
+                                order={section.order}
+                                allowRemoval={true}
+                            />
+                        )
+                    })
+                }
+                {
+                    (content?.[5]?.content?.cards?.length < 6) &&
+                    < button
+                        className="block text-blue-500 cursor-pointer my-3"
+                        onClick={() => addExtraSummary(indexes?.['5'], 5)}
+                    >
+                        Add Card...
+                    </button>
+                }
+            </div>
+
+        </div >
+    )
+}
+
+export default TemplateFourManager
--- dashboard/src/features/Resources/components/websiteComponent/TemplateFour.jsx
+++ dashboard/src/features/Resources/components/websiteComponent/TemplateFour.jsx
@@ -28,13 +28,13 @@ const TemplateFour = ({ content, screen, language, width, highlight, liveContent
                             ${checkDifference(content?.['1']?.content?.images?.[0]?.url, liveContent?.['1']?.content?.images?.[0]?.url)}
                             flex items-center bg-cover bg-center ${isLeftAlign ? 'scale-x-[-1]' : ''}`}
                 style={{
-                    backgroundImage: `url("${
-                        // Img_url + content?.['1']?.content?.images?.[0]?.url
+                    backgroundImage: `url("${content?.['1']?.content?.images?.[0]?.url ?
+                        Img_url + content?.['1']?.content?.images?.[0]?.url :
                         services.contructionTowerImage
                         }")`,
                     backgroundPosition: "bottom",
                     height: isComputer ? getDynamicSize(600) : "70vh",
-                    padding: (isComputer) && `${getDynamicSize(100)} ${getDynamicSize(120)}`
+                    padding: (isComputer) && `${getDynamicSize(100)} ${getDynamicSize(123)}`
                 }}
             >
                 <div className="absolute inset-0 pointer-events-none z-0 flex items-center justify-end overflow-hidden">
@@ -74,7 +74,164 @@ const TemplateFour = ({ content, screen, language, width, highlight, liveContent
                     </div>
                 </div>
             </section>
-        </div>)
+
+            <section
+                style={{
+                    padding: (isComputer) && `${getDynamicSize(100)} ${getDynamicSize(145)}`
+                }}
+            >
+                <div className="flex "
+                    style={{
+                        gap: isComputer && getDynamicSize(20)
+                    }}
+                >
+                    <div className=""
+                        style={{
+                            width: isComputer && getDynamicSize(693),
+                        }}
+                    >
+                        <img src={
+                            content?.[2]?.content?.images?.[0]?.url ?
+                                Img_url + content?.[2]?.content?.images?.[0]?.url :
+                                projectPageData.asphaltWork
+                        } alt=""
+
+                            className="h-full"
+                        />
+                    </div>
+                    <div className="flex flex-col"
+                        style={{
+                            width: isComputer && getDynamicSize(503),
+                            gap: isComputer && getDynamicSize(20)
+                        }}
+                    >
+                        <img src={content?.[2]?.content?.images?.[1]?.url ?
+                            Img_url + content?.[2]?.content?.images?.[1]?.url :
+                            projectPageData.asphaltWork} alt="" className="aspect-[2/1] object-cover object-center" />
+                        <img src={content?.[2]?.content?.images?.[2]?.url ?
+                            Img_url + content?.[2]?.content?.images?.[2]?.url :
+                            projectPageData.asphaltWork} alt="" className="aspect-[2/1] object-cover object-center" />
+                    </div>
+                </div>
+            </section>
+
+            <section
+                className="px-10 py-10"
+                style={{
+                    padding: (isComputer || isTablet) && `${getDynamicSize(0)} ${getDynamicSize(150)}`
+                }}>
+                <section className={`flex gap-[10px] ${isPhone ? "flex-col p-1" : ""}`}>
+                    <h2 className={`text-[32px] flex-1 leading-[28px]
+                                                    ${checkDifference(content?.[2]?.content?.title?.[language], liveContent?.[2]?.content?.title?.[language])}
+                                                    `}
+                        style={{ fontSize: fontSize.experienceHeading, lineHeight: isComputer && getDynamicSize(40) }}
+                    >
+                        {
+                            content?.[3]?.content?.title?.[language] ||
+                            "Project Description"
+                        }
+                    </h2>
+                    <div className={`text-[9.5px] flex-1 ${fontLight}
+                                    ${checkDifference(content?.[2]?.content?.description?.[language], liveContent?.[2]?.content?.description?.[language])}
+                                    `}
+                        style={{ fontSize: fontSize.mainPara }}
+                        dangerouslySetInnerHTML={{
+                            __html:
+                                content?.[3]?.content?.description?.[language] ||
+                                "The scope of work for the project is to demolish, procure and Construct an IT Lab in Al-Midra Tower located in Dhahran."
+                        }}
+                    />
+                </section>
+            </section>
+
+            <section
+                className={`bg-[#00B9F20A]`}
+                style={{
+                    padding: (isComputer || isTablet) && `${getDynamicSize(25)} ${getDynamicSize(150)}`,
+                    margin: (isComputer || isTablet) && `${getDynamicSize(100)} ${getDynamicSize(0)}`
+                }}
+            >
+                <div className="flex-[1] flex flex-col"
+                    style={{ gap: getDynamicSize(30) }}
+                >
+                    <h2 className={`
+                    ${checkDifference(content?.['4']?.content?.title?.[language], liveContent?.['2']?.content?.title?.[language])}
+                    `}
+                        style={{ fontSize: fontSize.aboutMainPara, lineHeight: fontSize.headingLeading }}
+                    >
+                        {content?.['4']?.content?.title?.[language] ||
+                            "Lorem Ipsum"}
+                    </h2>
+
+                    <div
+                        style={{ fontSize: fontSize.mainPara }}
+                        className={`${fontLight} pr-3
+                                ${checkDifference(content?.['4']?.content?.description?.[language], liveContent?.['2']?.content?.description?.[language])}
+                        `}
+                        dangerouslySetInnerHTML={{
+                            __html: content?.['4']?.content?.description?.[language] ||
+                                "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that dffvdfvf dfj dsiwns vdjjs sssiiijee It is a long established fact that a reader will be distracted by the readable content of a page when fgdgdg It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that dffvdfvf dfj dsiwns vdjjs sssiiijee It is a long established fact that a reader will be distracted by the readable content of a page when fgdgdg It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that dffvdfvf dfj dsiwns vdjjs sssiiijee It is a long established fact that a reader will be distracted by the readable content of a page when fgdgdg"
+                        }}
+                    />
+
+                </div>
+            </section>
+
+
+            <section
+                style={{
+                    padding: (isComputer || isTablet) && `${getDynamicSize(0)} ${getDynamicSize(150)} ${getDynamicSize(100)}`
+                }}
+                className=""
+            >
+
+                <div className={`grid ${(isComputer) ? "grid-cols-3" : isTablet ? "grid-cols-2" : "grid-cols-1 p-10  gap-10"}`}
+                    style={{ gap: (isComputer || isTablet) && `${getDynamicSize(36)}` }}
+                >
+                    {
+                        (content?.[5]?.content?.cards || tempArr)?.map((e, i) => {
+                            return (
+                                <div className="bg-white flex flex-col border"
+                                    style={{
+                                        padding: isComputer ? `${getDynamicSize(16)}` : getDynamicSize(32),
+                                        gap: getDynamicSize(16)
+                                    }}
+                                    key={i}
+                                >
+                                    <div className="flex gap-4"
+                                        style={{ gap: (isComputer || isTablet) && getDynamicSize(10) }}
+                                    >
+                                        <img
+                                            src={
+                                                // aboutUsIcons.ourGoal
+                                                Img_url + e?.images?.[0]?.url
+                                            }
+                                            alt=""
+                                            style={{ width: (isComputer) && getDynamicSize(46) }}
+                                            className={`aspect-[1/1] w-[46px]`}
+                                        />
+                                        <h3
+                                            className=""
+                                            style={{ fontSize: fontSize.aboutMainPara }}
+                                        >
+                                            {e?.title?.[language] || "LOREM"}
+                                        </h3>
+                                    </div>
+                                    <p className={`${fontLight} text-[#718096]`}
+                                        style={{ fontSize: fontSize.mainPara }}
+                                    >
+                                        {e?.description?.[language] ||
+                                            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using  content"
+                                        }
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+        </div>
+    )
 }
 
 
